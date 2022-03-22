7436c2e9764c69683803033d6e6cd43b962daec4
Rajansundaram <73684488+Rajan-20@users.noreply.github.com>
2022-03-22 17:29:10 +0530

    Add files via upload

---
+++ Assignment/Operator.java
@@ -0,0 +1,19 @@
+package com.ob.homework;
+
+public class Operator {
+	public static void main(String[] args) {
+	//Left Shift Operator
+	int a= 6;
+	int b= a<<2;
+	System.out.println(b);//24
+	
+	//Right Shift Operator
+	int c = a>>2;
+	System.out.println(c);//1
+	
+	//Negation Operator
+	int d = 3;
+	System.out.println(~d);
+	
+	}
+}
