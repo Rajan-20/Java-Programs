f187ffa31bee9dcf51c189c36680141de25d8196
Rajansundaram <73684488+Rajan-20@users.noreply.github.com>
2022-03-23 09:17:12 +0530

    Add files via upload

---
+++ Assignment/ConstructorOverloading/Staff.java
@@ -0,0 +1,9 @@
+package com.ob.assignments;
+
+public class Staff {
+	public static void main(String[] args) {
+		Student student1 = new Student();
+		Student student2 = new Student("Rajubhai");	
+	}
+
+}
---
+++ Assignment/ConstructorOverloading/Student.java
@@ -0,0 +1,17 @@
+package com.ob.assignments;
+
+public class Student {
+	String name;
+	public Student() {
+		// TODO Auto-generated constructor stub
+		System.out.println("Unknown...");
+	}
+	public Student(String n) {
+		// TODO Auto-generated constructor stub
+		name = n;
+		System.out.println(name+" is the name of the student");
+	}
+	
+
+
+}
